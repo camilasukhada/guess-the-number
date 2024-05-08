0192262512e244f4039c0c6e63d6118556d97b4e
camilasukhada <camilasukhada@gmail.com>
2024-05-08 09:12:57 -0300

    iníicio de refatoracao do codigo

--- src/ComputerPlayer.java
+++ src/ComputerPlayer.java
@@ -1,2 +1,10 @@
-public class ComputerPlayer {
+public class ComputerPlayer extends Player {
+    @Override
+    public int makeGuess() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Jogador compuatdor, digite um número de 1 a 100: ");
+        //LÓGICA DE PEGAR PALPITE DO JOGADOR HUMANO.
+        return input.nextInt();
+    }
 }
+
