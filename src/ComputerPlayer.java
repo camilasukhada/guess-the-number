2be14f3b303a0ed5ec6872fa823ac4eadc1e61ab
camilasukhada <camilasukhada@gmail.com>
2024-05-08 09:54:02 -0300

    Começo da classe ComputerPlayer

--- src/ComputerPlayer.java
+++ src/ComputerPlayer.java
@@ -1,10 +1,7 @@
 public class ComputerPlayer extends Player {
     @Override
     public int makeGuess() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Jogador compuatdor, digite um número de 1 a 100: ");
-        //LÓGICA DE PEGAR PALPITE DO JOGADOR HUMANO.
-        return input.nextInt();
+        //LÓGICA DE PEGAR PALPITE DO JOGADOR COMPUTADOR.
     }
 }
 
